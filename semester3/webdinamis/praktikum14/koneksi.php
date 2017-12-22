148b8979286fd7fc090b99f049dd4764c5f5ed35
Muhamad Danil Rafiqi <danil.rafiqi@gmail.com>
2017-12-22 15:05:20 +0700

    Add files via upload

---
+++ semester3/webdinamis/praktikum14/koneksi.php
@@ -0,0 +1,8 @@
+<?php
+$host="localhost";
+$username="root";
+$password="";
+$databasename="mahasiswa_db";
+$koneksi=mysqli_connect($host,$username,$password, $databasename);
+
+?>
---
+++ semester3/webdinamis/praktikum14/percobaan14_1.php
@@ -0,0 +1,44 @@
+<html>
+<head>
+<title>.:: tes koneksi dengan server database ::.</title>
+</head>
+<body>
+<font face=verdana size=2>
+<h3>Tes koneksi dg mysql database server...</h3><p>
+<form>
+<input type=radio name=tombol Value=1>Sambung
+<input type=radio name=tombol Value=0>Putus
+<br><br>
+<input type=submit Value=" OK ">
+
+</form>
+<?
+require("koneksi.php");
+global $koneksi;
+global $databasename;
+
+$hub=$koneksi;
+if (isset($_GET['tombol']))
+{
+	$tombol = $_GET['tombol'];
+if ($tombol==1)
+{
+if ($hub)
+{
+echo ("Koneksi dg Server Database <b><u>SUKSES</b></u><br>");
+
+if ($hub) echo "Database <b><u>$databasename</b></u> ditemukan";
+else
+echo "Database <b><u>$databasename</b></u> TIDAK ditemukan";
+} else
+echo ("Koneksi dg Server Database <b><u>GAGAL</b></u><br>");
+}
+elseif ($tombol==0)
+{
+$mati=@mysqli_close($koneksi);
+echo ("Koneksi Server Database <b><u>Dimatikan</b></u><br>");
+}
+}
+?>
+</body>
+</html>
